d1a95d222f2311dcc2985ae05aabb2a4b07730ba
Jugal Patel <jugal.patel@talentslist.com>
2024-04-13 11:11:42 +0530

    Updating changes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\check_login;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-route::post('/login',[AuthController::class,'login']);
+route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => [check_login::class]], function () {
+    Route::post("logout", [AuthController::class, "logout"])->name('logout');
+});
